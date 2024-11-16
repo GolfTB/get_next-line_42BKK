a08838956ed1b3fa118a8a9fe8e212b21f27c952
GolfTB <golf301046@gmail.com>
2024-11-16 12:49:18 +0700

    manda

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
+#include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "get_next_line.h"
 
-int main(void)
+int	main(void)
 {
-    int fd;
-    char *line;
+	int		fd;
+	char	*line;
 
-    fd = open("test.txt", O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error opening file");
-        return (1);
-    }
-
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-
-    close(fd);
-    return (0);
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
